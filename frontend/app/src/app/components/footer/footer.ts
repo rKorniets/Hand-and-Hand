20e04353f5db6000a57bf02cc155b69297cde0e0
SkyterYS <skyterys49@gmail.com@example.com>
2026-03-13 21:49:34 +0200

    Fix review comments

--- frontend/app/src/app/components/footer/footer.ts
+++ frontend/app/src/app/components/footer/footer.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './_footer.scss',
+  styleUrl: './footer.scss',
 })
 export class Footer {}
 
--- frontend/app/src/app/components/header/header.ts
+++ frontend/app/src/app/components/header/header.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './_header.scss',
+  styleUrl: './header.scss',
 })
 export class Header {}
