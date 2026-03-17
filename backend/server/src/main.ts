1e688784870b469aa0a39ee7965b9d4ab4fcc3d3
advptive <denis.mor.usp@gmail.com>
2026-03-18 00:42:58 +0200

    add parameter skip and OpenAPI(Swagger)

--- backend/server/src/main.ts
+++ backend/server/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Hand-and-Hand API')
+    .setDescription('документація для бекенду')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
