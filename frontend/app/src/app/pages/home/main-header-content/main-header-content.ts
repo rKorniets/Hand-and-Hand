d6e1e74f1b7532c657aaa69ca499220ad5d9a757
SkyterYS <skyterys49@gmail.com@example.com>
2026-03-20 18:44:53 +0200

    feature: news page with API, pagination, pinned scroll + ui updates

--- frontend/app/src/app/pages/home/main-header-content/main-header-content.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-main-header-content',
-  imports: [],
-  templateUrl: './main-header-content.html',
-  styleUrl: './main-header-content.scss',
-})
-export class MainHeaderContent {}
