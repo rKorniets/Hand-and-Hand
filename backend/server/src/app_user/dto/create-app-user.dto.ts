0362128ee0655fd9d9fe47ddca01a094c164e937
Nazarii_Vykovych <nazarko1706@gmail.com>
2026-03-20 14:21:48 +0200

    don't love my fast suggestions (Idk how to do it right so just reverted password to password_hash back((need to read smth about it)) )

--- backend/server/src/app_user/dto/create-app-user.dto.ts
+++ backend/server/src/app_user/dto/create-app-user.dto.ts
@@ -2,7 +2,7 @@ import { user_role_enum, user_status_enum } from '@prisma/client';
 
 export class CreateAppUserDto {
   email: string;
-  password: string;
+  password_hash: string;
   role: user_role_enum;
   // When omitted, the service defaults this to user_status_enum.PENDING
   status?: user_status_enum;
