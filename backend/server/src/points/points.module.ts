0abbc67ebedc1eae5d1b42f3ab4311dbcabcd5df
advptive <denis.mor.usp@gmail.com>
2026-04-26 02:03:18 +0300

    feat(points): implement points transaction system and API

--- backend/server/src/points/points.module.ts
+++ backend/server/src/points/points.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PointsService } from './points.service';
+import { PointsController } from './points.controller';
 
 @Module({
+  controllers: [PointsController],
   providers: [PointsService],
   exports: [PointsService],
 })
