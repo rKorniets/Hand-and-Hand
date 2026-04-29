fa712409e56016d0e09c6b1b16bfd8e6763ce4f9
Sofia <sofikononova1@gmail.com>
2026-04-29 15:24:53 +0300

    Resolve merge conflicts

--- backend/server/src/project/project.module.ts
+++ backend/server/src/project/project.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CloudinaryModule],
   controllers: [ProjectController],
   providers: [ProjectService],
 })
